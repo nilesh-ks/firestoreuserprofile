e6a80cd3d0d4588e3beae4314f6ad0bf4328b30a
Project <nileshsinha1731@gmail.com>
2021-04-09 23:57:45 +0530

    Initial commit

---
+++ app/src/main/java/com/android/UserProfile/ShowProfile.java
@@ -0,0 +1,14 @@
+package com.android.UserProfile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShowProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_profile);
+    }
+}
